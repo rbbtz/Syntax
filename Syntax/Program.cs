7f8e2ad4a6d9224308a638f7fb98128d2b30feec
rbbtz <robbetz@gmail.com>
2020-08-26 08:21:32 -0400

    Syntax

---
+++ Syntax/Program.cs
@@ -0,0 +1,30 @@
+﻿// using System;
+
+// namespace Syntax //
+//{
+// class Program
+//{
+//static void Main(string[] args)
+//{
+//Console.WriteLine("Hello World!");
+//       }
+//   }
+//}
+
+
+//int answer = 4;
+//string response;
+
+//if (answer < 9)
+//{
+//response = answer + " is less than nine";
+//}
+//else
+//{
+//response = answer + “greater than or equal to nine”; 
+//} //
+
+var answer = 4;
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer}" is greater than or equal to nine";
+
+Console.WriteLine(response);
